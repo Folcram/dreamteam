702049685cdbc03721d010f3af3cf2528b4f0ca2
Sm21Mapgapita11957 <margo.smirnova.112404@mail.ru>
2022-12-23 22:49:53 +0300

    QuickSort

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <chrono>
+using namespace std;
+
+
+int partition(int* a, int start, int end)
+{
+    int pivot = a[end];
+
+    int pIndex = start;
+
+
+    for (int i = start; i < end; i++)
+    {
+        if (a[i] <= pivot)
+        {
+            swap(a[i], a[pIndex]);
+            pIndex++;
+        }
+    }
+
+
+    swap(a[pIndex], a[end]);
+    return pIndex;
+}
+
+void quicksort(int* a, int start, int end)
+{
+   
+    if (start >= end) {
+        return;
+    }
+
+   
+    int pivot = partition(a, start, end);
+
+    quicksort(a, start, pivot - 1);
+
+    
+    quicksort(a, pivot + 1, end);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int* a = new int(n);
+    for (int i = 0; i < n; i++) {
+        a[i] = rand();
+    }
+    quicksort(a, 0, n - 1);
+
+    // печатаем отсортированный массив
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+
+    return 0;
+}
