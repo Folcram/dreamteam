6e04b245c650b47d81a2edcf54956173c422a523
Folcram <120056473+Folcram@users.noreply.github.com>
2022-12-25 15:19:15 +0300

    Update Sort.cpp

--- Sort.cpp
+++ Sort.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 using namespace std;
+//Сортировка Шелла
 void shellSort(int* arr, const int& n)
 {
-    for (int l = n / 2; l > 0; l /= 2)
+    for (int shag = n / 2; shag > 0; shag /= 2)
     {
-        for (int i = l; i < n; i++)
+        for (int i = shag; i < n; i++)
         {
             int temp = arr[i];
             int j;
-            for (j = i; j >= l && arr[j - l] > temp; j -= l)
+            for (j = i; j >= shag && arr[j - l] > temp; j -= shag)
             {
-                arr[j] = arr[j - l];
+                arr[j] = arr[j - shag];
             }
             arr[j] = temp;
         }
     } 
 }
-void merge(int* arr, int low, int high, int mid)
+// доп. функция для сортировки слиянием п
+void merge(int* arr, int low, cont int& high, int mid)
 {
     int i, j, k;
     int* c = new int[high];
@@ -28,12 +30,14 @@ void merge(int* arr, int low, int high, int mid)
         if (arr[i] < arr[j]) 
         {
             c[k] = arr[i];
+            //эти счетчики необходимы для смены индекса элемента сотрируемого массива
             k++;
             i++;
         }
         else 
         {
             c[k] = arr[j];
+            //эти счетчики необходимы для смены индекса элемента сотрируемого массива
             k++;
             j++;
         }
@@ -55,6 +59,7 @@ void merge(int* arr, int low, int high, int mid)
         arr[i] = c[i];
     }
 }
+// сортировка слиянием
 void merge_sorting(int* arr, int low, int high)
 {
     int mid;
@@ -66,6 +71,7 @@ void merge_sorting(int* arr, int low, int high)
         merge(arr, low, high, mid);
     }
 }
+// сортировка вставками
 void InsertionSort(int* arr, const int& n)
 {
     for (int k = 1; k < n; k++)
@@ -80,6 +86,7 @@ void InsertionSort(int* arr, const int& n)
         arr[j + 1] = temp;
     }
 }
+// доп. функция для быстрой сортировки
 int partition(int* arr, const int& start,const int& end)
 {
     int pivot = arr[end];
@@ -95,6 +102,7 @@ int partition(int* arr, const int& start,const int& end)
     swap(arr[pIndex], arr[end]);
     return pIndex;
 }
+// функция  быстрой сортировки
 void quicksort(int* arr, int start, int end)
 {
     if (start >= end) 
@@ -105,6 +113,7 @@ void quicksort(int* arr, int start, int end)
     quicksort(arr, start, pivot - 1);
     quicksort(arr, pivot + 1, end);
 }
+//доп. функция  пирамидальной сортировки
 void heapify(int* arr, int n, int root)
 {
     int largest = root;
@@ -120,6 +129,7 @@ void heapify(int* arr, int n, int root)
         heapify(arr, n, largest);
     }
 }
+// функция  пирамидальной сортировки
 void heapSort(int* arr, const int& n)
 {
     for (int i = n / 2 - 1; i >= 0; i--) 
@@ -138,6 +148,7 @@ void heapSort(int* arr, const int& n)
 }
 int main()
  {
+         // Рандомим размер и элементы массива
         int size = rand();
         int* arr = new int[size];
         for (int i = 0; i < size(); i++) 
@@ -152,6 +163,7 @@ int main()
         cout << "3 - InsertionSort" << end;
         cout << "4 - MergeSort" << endl;
         cout << "5 - ShellSort" << endl;
+        // В зависимости от набраного numbermenu в switch-case вызвается функция сортировки
         cin >> numbermenu;
         switch (numbermenu) 
         {
@@ -180,6 +192,7 @@ int main()
                 shellSort(arr, size);
                 break;
             }
+            // если выбран не тот номер то вызывается ошибка
             default 
             {
                 cout << "Ошибка" << endl;
