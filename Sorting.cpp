3c169eb69b8a87c699d39f5edccb561464f18694
Folcram <120056473+Folcram@users.noreply.github.com>
2022-12-25 12:58:25 +0300

    Update and rename ConsoleApplication9.cpp to Sorting.cpp

--- Sorting.cpp
+++ Sorting.cpp
@@ -4,6 +4,20 @@
 using namespace std;
 
 
+void quicksorting(){
+    int n;
+    cin >> n;
+    int* a = new int[n];
+    for (int i = 0; i < n; i++) {
+        a[i] = rand();
+    }
+    quicksort(a, 0, n - 1);
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+
+}
+
 int partition(int* a, int start, int end)
 {
     int pivot = a[end];
@@ -43,18 +57,7 @@ void quicksort(int* a, int start, int end)
 
 int main()
 {
-    int n;
-    cin >> n;
-    int* a = new int(n);
-    for (int i = 0; i < n; i++) {
-        a[i] = rand();
-    }
-    quicksort(a, 0, n - 1);
-
-    // печатаем отсортированный массив
-    for (int i = 0; i < n; i++) {
-        cout << a[i] << " ";
-    }
-
-    return 0;
+    
+quicksorting();
+   
 }
